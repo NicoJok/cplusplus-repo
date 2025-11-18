a212cb3cf83a799293dafcb9c503fa9ce78da219
Nico Jokinen <nico.jokinen96@gmail.com>
2025-11-18 03:49:45 +0200

    1-3

---
+++ tuntitehtävät7/tehtävä3.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Auto
+{
+private:
+    string rekisteritunnus;
+    int huippunopeus;
+    int nopeus;
+    double matka;
+
+public:
+    Auto(string rekkari, int huippu) : rekisteritunnus(rekkari), huippunopeus(huippu), nopeus(0), matka(0) {}
+
+    void kiihdy(int muutos)
+    {
+        nopeus += muutos;
+
+        if (nopeus > huippunopeus)
+        {
+            nopeus = huippunopeus;
+        }
+        if (nopeus < 0)
+        {
+            nopeus = 0;
+        }
+    }
+
+    void kulje(double tunnit) {
+        matka += nopeus * tunnit;
+    }
+
+    void tiedot()
+    {
+        cout << "Rekkari: " << rekisteritunnus << "\n";
+        cout << "Huippunopeus: " << huippunopeus << " km/h" << "\n";
+        cout << "Nopeus: " << nopeus << " km/h" << "\n";
+        cout << "Kuljettu matka: " << matka << " km" << "\n";
+    }
+};
+
+int main()
+{
+    Auto auto1("ABC-123", 142);
+    auto1.kiihdy(30);
+    auto1.kiihdy(70);
+    auto1.kiihdy(50);
+
+    cout << "Nopeus:" << "\n";
+    auto1.tiedot();
+    cout << "\n" << "Ajomatka" << "\n";
+    auto1.kulje(1.5);
+    cout << "\n" << "Jarrutus" << "\n";
+    auto1.kiihdy(-200);
+    cout << "\n" << "Nopeus:" << "\n";
+    auto1.tiedot();
+
+    return 0;
+}
